95f05c887bc4bc919fd9cd20e032ba49c930f612
Paulo Victor <paulovictorce@hotmail.com>
2022-11-23 09:30:31 -0300

    [TrainStation] Project configuration and swagger implementation.

---
+++ src/main/java/com/trainstation/TrainstationApplication.java
@@ -0,0 +1,13 @@
+package com.trainstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainstationApplication.class, args);
+	}
+
+}
